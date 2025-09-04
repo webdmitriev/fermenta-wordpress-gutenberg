1bfae7e1d853f4e3c1686357445cec2697f29145
webdmitriev <webdmitriev@gmail.com>
2025-09-04 16:44:51 +0300

    Changed some files (blocks and scss

--- fermenta-gutenberg/blocks/block-05.php
+++ fermenta-gutenberg/blocks/block-05.php
@@ -10,10 +10,12 @@ $allowed_tags = array(
   'br'   => array()
 );
 
+$isDark = get_field("is_dark_or_light");
+
 ?>
 
 <!-- <?= $block_path; ?> (start) -->
-<section class="services">
+<section class="services" style="background-color: <?= $isDark ? '#b0a195' : '#8ca086'; ?>">
   <?php if( is_admin() ) : ?>
     <style>[data="gutenberg-preview-img"] img {width: 100%;object-fit: contain;}</style>
     <div class="gutenber-block" style="padding: 10px 20px;background-color: #F5F5F5;border: 1px solid #D1D1D1;"><?= $gutenberg_title; ?></div>
@@ -24,10 +26,10 @@ $allowed_tags = array(
     <div class="container">
       <div class="line-wrap df-fs-st w-100p">
         <?php if( have_rows('programs') ) : while ( have_rows('programs') ) : the_row(); ?>
-          <div class="services-item">
-            <?php if(get_sub_field('title')): ?><span class="services-item__title"><?php echo wp_kses(get_sub_field('title'), $allowed_tags); ?></span><?php endif; ?>
-            <?php if(get_sub_field('descr')): ?><span class="services-item__descr"><?php echo wp_kses(get_sub_field('descr'), $allowed_tags); ?></span><?php endif; ?>
-            <?php if(get_sub_field('btn_text')): ?><button class="btn btn-gray"><?php echo wp_kses(get_sub_field('btn_text'), $allowed_tags); ?></button><?php endif; ?>
+          <div class="services-item <?= get_sub_field('btn_text') ? '' : 'without-btn'; ?>" style="background-color: <?= $isDark ? '#dacdc2' : '#b2c4ad'; ?>">
+            <?php if(get_sub_field('title')): ?><span class="services-item__title" style="color: <?= $isDark ? '#493a2f' : '#6f7e73'; ?>"><?php echo wp_kses(get_sub_field('title'), $allowed_tags); ?></span><?php endif; ?>
+            <?php if(get_sub_field('descr')): ?><span class="services-item__descr" style="color: <?= $isDark ? '#493a2f' : '#6f7e73'; ?>"><?php echo wp_kses(get_sub_field('descr'), $allowed_tags); ?></span><?php endif; ?>
+            <?php if(get_sub_field('btn_text')): ?><button class="btn btn-gray" style="color: <?= $isDark ? '#493a2f' : '#6f7e73'; ?>;background-color: <?= $isDark ? '#b0a195' : '#b2c4ad'; ?>"><?php echo wp_kses(get_sub_field('btn_text'), $allowed_tags); ?></button><?php endif; ?>
           </div>
         <?php endwhile; endif; ?>
       </div>
