21a8da76bedad3f848e50f7e3cd1e3bd21dda7e0
webdmitriev <webdmitriev@gmail.com>
2025-09-06 17:14:24 +0300

    Added funcs acf for change social media (footer and fixed menu)

--- fermenta-gutenberg/admin.php
+++ fermenta-gutenberg/admin.php
@@ -9,3 +9,17 @@ add_filter ( 'show_admin_bar', '__return_false');
  * wpcf7 remove defaults <br/>
  */
 add_filter('wpcf7_autop_or_not', '__return_false');
+
+/**
+ * Options page
+ */
+if( function_exists('acf_add_options_page') ) {
+  acf_add_options_page(array(
+    'page_title' => 'Options page',
+    'menu_title' => 'Options page',
+    'menu_slug' => 'theme-general-settings',
+    'capability' => 'edit_posts',
+    'update_button' => __('Update', 'acf'),
+    'redirect' => false
+  ));
+}
